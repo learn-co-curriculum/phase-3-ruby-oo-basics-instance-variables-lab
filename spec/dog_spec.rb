f30fb76736a968847533656fcb35112f5e2864e3
Danny-Sullivan <dsully360@gmail.com>
2015-11-02 11:44:31 -0500

    updated specs

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -5,22 +5,23 @@ describe 'Dog' do
     expect(defined?(Dog)).to be_truthy
     expect(Dog).to be_a(Class)
   end
-end
 
-describe 'name=' do
-  it 'defines an instance variable @this_dogs_name' do
-    lassie = Dog.new
-    lassie.name = 'Lassie'
+  describe 'name=' do
+    it 'defines an instance variable @this_dogs_name' do
+      lassie = Dog.new
+      lassie.name = 'Lassie'
 
-    expect(lassie.instance_variable_get("@this_dogs_name")).to eq('Lassie')
+      expect(lassie.instance_variable_get("@this_dogs_name")).to eq('Lassie')
+    end
   end
-end
 
-describe 'name' do
-  it 'defines an instance variable @this_dogs_name' do
-    lassie = Dog.new
-    lassie.instance_variable_set("@this_dogs_name","Lassie")
+  describe 'name' do
+    it 'defines an instance variable @this_dogs_name' do
+      lassie = Dog.new
+      lassie.instance_variable_set("@this_dogs_name","Lassie")
 
-    expect(lassie.name).to eq("Lassie")
+      expect(lassie.name).to eq("Lassie")
+    end
   end
+
 end
