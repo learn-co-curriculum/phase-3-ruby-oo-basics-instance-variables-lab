948d90da363f4926016a6a58719106bb1a0eb1f2
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2015-12-03 09:44:53 -0500

    clarify it block descriptions

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -16,7 +16,7 @@ describe 'Dog' do
   end
 
   describe 'name' do
-    it 'defines an instance variable @this_dogs_name' do
+    it 'retrieves the value of an instance variable @this_dogs_name' do
       lassie = Dog.new
       lassie.instance_variable_set("@this_dogs_name","Lassie")
 
