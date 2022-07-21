36d382a9b8f8d60fe506502c02f2f8a7ed1a4b41
NickeaBennett <nickeabennett@gmail.com>
2022-07-21 05:37:00 -0400

    commit tree df48c1a05e340f363a1098edeb712d32b6039bdb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+
+puts lassie.name
