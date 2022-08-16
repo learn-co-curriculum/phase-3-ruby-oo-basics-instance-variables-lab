df23f463a48e26d7372ff8982ae8d232497b76d2
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-16 17:04:59 +0300

    commit tree 0a45563e7f85ba242071f9519bb3f6533ea73caf

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+      end
+    
+      def name
+        @this_dogs_name
+      end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
