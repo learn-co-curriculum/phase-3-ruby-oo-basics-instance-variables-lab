f0e57f96baeb21af2e1ffe823b04b010f59afd2f
+ <+>
2023-02-09 10:47:30 +0300

    commit tree 4177227fb6089ff9f29ee81800282ff1bbb92c11

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+  
+  
+  
