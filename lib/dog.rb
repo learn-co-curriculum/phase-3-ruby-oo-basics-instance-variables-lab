359280274839269347b2b681853e20d0e95f50bf
WendyNjuguna <wendynjuguna714@gmail.com>
2023-02-10 02:30:59 +0300

    commit tree 92bc8e18b0702097357a9a131ca204084df55674

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-
-end
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
