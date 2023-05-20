c1138c54d48646be9ceb1b5b7eefea76d6c202c6
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-05-19 19:13:57 -0700

    instance variables

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,36 @@
 class Dog
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+
+# class Dog
+#     # setter method
+#     def name=(value)
+#       @name = value
+#     end
+  
+#     # getter method
+#     def name
+#       # access the data from the @name instance variable and return it
+#       @name
+#     end
+#   end
+  
+#   odie = Dog.new
+#   odie.name = "Odie"
+  
+#   # call the Dog#name method
+#   puts odie.name
+#   # "Odie"
